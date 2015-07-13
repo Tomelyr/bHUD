96fb0fff7b298f8dbfe4f9f1ecb4ff4d4af66e7f
Tomelyr <Tomelyr@googlemail.com>
2015-07-13 04:15:29 +0200

    Update cl_bhud.lua

--- lua/darkrp_modules/bhud/cl_bhud.lua
+++ lua/darkrp_modules/bhud/cl_bhud.lua
@@ -69,6 +69,7 @@ local LP = LocalPlayer()
 
 local function getAmmo()
 	if !LP:Alive() then return -1 end
+	if !IsValid(LP:GetActiveWeapon()) then return "" end
 	local weap = LP:GetActiveWeapon()
 	if not weap then return 0 end
 	if weap:GetClass() == 'weapon_physgun' or weap:GetClass() == 'weapon_physcannon' then return "" end
